7fdc67fa51372746f8b95feba934aa25e91206b2
napoleaofw <napoleao.wingert@gmail.com>
2017-12-31 21:04:37 -0200

    change route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { RecordsAccountComponent } from './records-account/records-account.component';
 import { RecordsCategoryComponent } from './records-category/records-category.component';
+import { RecordsInstallmentComponent } from './records-installment/records-installment.component';
 import { RecordsPersonComponent } from './records-person/records-person.component';
 import { RecordsTransactionComponent } from './records-transaction/records-transaction.component';
 
@@ -10,6 +11,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/accounts', pathMatch: 'full' },
   { path: 'accounts', component: RecordsAccountComponent },
   { path: 'categories', component: RecordsCategoryComponent },
+  { path: 'installments', component: RecordsInstallmentComponent },
   { path: 'persons', component: RecordsPersonComponent },
   { path: 'transactions', component: RecordsTransactionComponent }
 ];
