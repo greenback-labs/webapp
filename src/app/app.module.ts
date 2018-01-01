ad5fd7e9ba57b23250b57125155a9302694fbc24
napoleaofw <napoleao.wingert@gmail.com>
2018-01-01 20:29:30 -0200

    added component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AccountListComponent } from './account/account-list/account-list.component';
+import { AccountFormComponent } from './account/account-form/account-form.component';
+import { CategoryFormComponent } from './category/category-form/category-form.component';
+import { CategoryListComponent } from './category/category-list/category-list.component';
+import { PersonListComponent } from './person/person-list/person-list.component';
+import { PersonFormComponent } from './person/person-form/person-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccountListComponent,
+    AccountFormComponent,
+    CategoryFormComponent,
+    CategoryListComponent,
+    PersonListComponent,
+    PersonFormComponent
   ],
   imports: [
     BrowserModule
